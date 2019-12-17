137106962fb04da52063837c2ada11006e6a320d
Milad <milad.bourhani@gmail.com>
2019-12-17 21:40:11 +0100

    types

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,16 @@ import System.Environment
 
 type Parser = Parsec String ()
 
-data Foo = Foo String Char deriving Show
+data Foo = Foo String Char
+  deriving Show
 
+f :: Parser Char
 f = char 'f' :: Parser Char
+
+n :: Parser String
 n = many f
+
+l :: Parser Foo
 l = do
   name <- n
   d <- digit
